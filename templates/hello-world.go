4752c85a0e0bb0ab036d115eb5c30091ebb45fa9
Maciej Papież <maciej.papiez@boldare.com>
2019-01-30 16:02:36 +0100

    Remove logo and social links

--- templates/hello-world.go
+++ templates/hello-world.go
@@ -15,7 +15,7 @@ const (
         font-family: "Open Sans","Helvetica Neue",Helvetica,Arial,sans-serif;
       }
       button {
-          background-color: #0075a8; 
+          background-color: #0075a8;
           border: none;
           color: white;
           padding: 15px 32px;
@@ -31,11 +31,10 @@ const (
     </style>
   </head>
   <body>
-    <img id="logo" src="img/rancher-logo.svg" alt="Rancher logo" width=400 />
     <h1>Hello world!</h1>
     <h3>My hostname is {{.Hostname}}</h3>`
 
-	webServices = `{{- $length := len .Services }} 
+	webServices = `{{- $length := len .Services }}
   {{- if gt $length 0 }}
     <div id='Services'>
       <h3>k8s services found {{$length}}</h3>
@@ -57,16 +56,6 @@ const (
     </div>
     <br />`
 
-	webLinks = `    <div id='rancherLinks' class="row social">
-      <a class="p-a-xs" href="https://rancher.com/docs"><img src="img/favicon.png" alt="Docs" height="25" width="25"></a>
-      <a class="p-a-xs" href="https://slack.rancher.io/"><img src="img/icon-slack.svg" alt="slack" height="25" width="25"></a>
-      <a class="p-a-xs" href="https://github.com/rancher/rancher"><img src="img/icon-github.svg" alt="github" height="25" width="25"></a>
-      <a class="p-a-xs" href="https://twitter.com/Rancher_Labs"><img src="img/icon-twitter.svg" alt="twitter" height="25" width="25"></a>
-      <a class="p-a-xs" href="https://www.facebook.com/rancherlabs/"><img src="img/icon-facebook.svg" alt="facebook" height="25" width="25"></a>
-      <a class="p-a-xs" href="https://www.linkedin.com/groups/6977008/profile"><img src="img/icon-linkedin.svg" height="25" alt="linkedin" width="25"></a>
-    </div>
-    <br />`
-
 	webTail = `    <script>
       function myFunction() {
           var x = document.getElementById("` + reqInfoID + `");
@@ -82,7 +71,6 @@ const (
 
 	HelloWorldTemplate = webHead + `
 ` + webServices + `
-` + webLinks + `
 ` + webDetails + `
 ` + webTail
 )
