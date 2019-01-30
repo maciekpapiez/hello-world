05b33dc3f7e5808779b7c5582c2a0a370b721713
Maciej Papież <maciej.papiez@boldare.com>
2019-01-30 17:10:24 +0100

    Add TravisCI deployment

---
+++ scripts/build-and-push-image.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -e
+
+REPOSITORY_URL=${REPO}
+IMAGE_NAME="hello-world"
+IMAGE_VERSION=${TRAVIS_COMMIT::7} # that's git rev-parse --short HEAD format
+
+docker --version
+
+echo "Installing aws-cli..."
+pip install awscli --user
+export PATH=~/.local/bin:$PATH
+
+echo "Logging in to ECR..."
+$(aws ecr get-login --no-include-email --region eu-west-1)
+
+# echo "Building the image..."
+# docker build . -f ./docker/k8s/Dockerfile -t $IMAGE_NAME
+
+# echo "Tagging the image..."
+# docker tag $IMAGE_NAME $REPOSITORY_URL/$IMAGE_NAME:$IMAGE_VERSION
+# docker tag $IMAGE_NAME $REPOSITORY_URL/$IMAGE_NAME:$TRAVIS_COMMIT
+# docker tag $IMAGE_NAME $REPOSITORY_URL/$IMAGE_NAME:$TRAVIS_BRANCH
+
+echo "Pushing the image to the remote repository..."
+# docker push $REPOSITORY_URL/$IMAGE_NAME:$IMAGE_VERSION
+docker push $REPOSITORY_URL/$IMAGE_NAME:$TRAVIS_COMMIT
+# docker push $REPOSITORY_URL/$IMAGE_NAME:$TRAVIS_BRANCH
+
+echo "Done!"
